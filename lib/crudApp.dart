a6abb0808902398e9bba4abaffaf50af7636f9f0
siraajul <shirajulislamparvez@gmail.com>
2024-12-15 09:14:45 +0600

    Update App name

--- lib/crudApp.dart
+++ lib/crudApp.dart
@@ -4,8 +4,8 @@ import 'package:crud/ui/screens/product_list_screen.dart';
 import 'package:crud/ui/screens/update_product_screen.dart';
 import 'package:flutter/material.dart';
 
-class CRUDApp extends StatelessWidget {
-  const CRUDApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shimmer/main.dart';
+import 'crudApp.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
